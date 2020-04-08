852537cd2782c824a3d094b294e46a0f23669368
jiayu520 <jiayu520000@outlook.com>
2020-04-08 17:48:04 +0800

    企业微信

--- page/reuse.py
+++ page/reuse.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
-from _ast import List, Dict
-from asyncio import sleep
-from time import time
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -19,15 +17,21 @@ class TestTest():
     #     self.driver.quit()
 
     def test_test(self):
+        time.sleep(3)
+        #self.driver.find_element(By.CSS_SELECTOR,".index_service_cnt_item_title").click()#这步会在已经登录的企业微信界面操作
 
-        self.driver.find_element(By.CSS_SELECTOR,".index_service_cnt_item_title").click()#这步会在已经登录的企业微信界面操作
-
-        cookies = self.driver.get_cookies()
-        with open("cookies.txt",'w') as f:
-            json.dump(cookies,f)#将cookie存入txt中
-        # with open("cookies.txt",'r') as f:
-        #     cookies:list[dict]=json.load(f)#将TXT中的内容加载到f,前面的作用是告诉变量的数据类型，cookie是一个列表里面包的字典，后续调用是输入点就会有list的方法
-        #     for cookie in cookies:
-        #         self.driver.add_cookie(cookie)
-        #         print(cookie)
-        # self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
+        # cookies = self.driver.get_cookies()
+        # with open("cookies.txt",'w') as f:
+        #     json.dump(cookies,f)#将cookie存入txt中
+        with open("cookies.txt",'r') as f:
+            cookies:list[dict]=json.load(f)#将TXT中的内容加载到f,前面的作用是告诉变量的数据类型，cookie是一个列表里面包的字典，后续调用是输入点就会有list的方法
+            for cookie in cookies:
+                self.driver.add_cookie(cookie)
+                #print(cookie)
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
+        time.sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR,".index_service_cnt_item_title").click()
+        time.sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR,"#username").send_keys("hello")
+        time.sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR,"#memberAdd_acctid").send_keys("12334")
